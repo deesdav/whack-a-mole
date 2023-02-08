28580cd4d4143f0c241a25fa2d9294f7ca8cd332
deesdav <105449474+deesdav@users.noreply.github.com>
2023-02-08 08:23:35 +0100

    update

--- res/js/main.js
+++ res/js/main.js
@@ -1,14 +1,17 @@
 const play = document.getElementById("play");
+const grid = document.getElementById("grid");
 const backButton = document.getElementById("backButton");
 const mainStartingContent = document.getElementById("mainStartingContent");
 
 play.onclick = () => {
     mainStartingContent.style.display = "none";
     backButton.style.display = "block";
+    grid.style.display = "flex";
     document.body.style.backgroundImage = "none";
 }
 backButton.onclick = () => {
     mainStartingContent.style.display = "flex";
     backButton.style.display = "none";
+    grid.style.display = "none";
     document.body.style.backgroundImage = "url(./res/img/mainBackground.jfif)";
 }
