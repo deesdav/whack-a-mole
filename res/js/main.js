8e3f7fd15c8a41ef53685eb0c991d90b665470a4
deesdav <105449474+deesdav@users.noreply.github.com>
2023-02-13 15:05:22 +0100

    update

--- res/js/main.js
+++ res/js/main.js
@@ -11,48 +11,43 @@ const gameArea9x9 = document.getElementById("gameArea9x9");
 const lunar = document.querySelector(".lunar");
 const score = document.querySelector("#score");
 
+
 musicButton.onmousedown = () => {
   music.src = "https://www.youtube.com/embed/pDKvYBTZ1i4?autoplay=1";
-
-}
+};
 musicButton.onmouseover = () => {
   music.src = "";
- 
-}
+};
 play.onclick = () => {
-    mainStartingContent.style.display = "none";
-    backButton.style.display = "block";
-    scoreText.style.display = "block";
-    gameArea9x9.style.display = "flex";
-    document.body.style.backgroundImage = "none";
-    gameBox.style.display = "block";
-   
-   
-}
+  mainStartingContent.style.display = "none";
+  backButton.style.display = "block";
+  scoreText.style.display = "block";
+  gameArea9x9.style.display = "flex";
+  document.body.style.backgroundImage = "none";
+  gameBox.style.display = "block";
+};
 backButton.onclick = () => {
-    mainStartingContent.style.display = "flex";
-    backButton.style.display = "none";
-    scoreText.style.display = "none";
-    gameArea9x9.style.display = "none";
-   
-    document.body.style.backgroundImage = "url(./res/img/background.gif)";
-}
+  mainStartingContent.style.display = "flex";
+  backButton.style.display = "none";
+  scoreText.style.display = "none";
+  gameArea9x9.style.display = "none";
 
-
+  document.body.style.backgroundImage = "url(./res/img/background.gif)";
+};
 
 let result = 0;
 let strikePosi;
 let timerId = 0;
 
-function randomround() {
+function randomRound() {
   rounds.forEach((round) => {
     round.classList.remove("lunar");
   });
 
-  const randomround = rounds[Math.floor(Math.random() * 9)];
-  randomround.classList.add("lunar");
+  const randomRound = rounds[Math.floor(Math.random() * 9)];
+  randomRound.classList.add("lunar");
 
-  strikePosi = randomround.id;
+  strikePosi = randomRound.id;
 }
 
 rounds.forEach((round) => {
@@ -63,18 +58,17 @@ rounds.forEach((round) => {
       result += 10;
       score.innerHTML = result;
       strikePosi = 0;
- 
     }
     if (result > 300) {
-        alert("You reached score above 300, so now, if you want to play aqain click on 'ok' and reload the page.");
-        mainStartingContent.style.display = "flex";
-        backButton.style.display = "none";
-        scoreText.style.display = "none";
-        gameArea9x9.style.display = "none";
-        document.body.style.backgroundImage = "url(./res/img/background.gif)";
-
+      alert(
+        "You reached score above 300, so now, if you want to play aqain click on 'ok' and reload the page."
+      );
+      mainStartingContent.style.display = "flex";
+      backButton.style.display = "none";
+      scoreText.style.display = "none";
+      gameArea9x9.style.display = "none";
+      document.body.style.backgroundImage = "url(./res/img/background.gif)";
     }
-   
   });
 });
 
@@ -82,12 +76,11 @@ let clickToFast = 500;
 
 rounds.onclick = () => {
   clickToFast += 10;
+};
+
+function moveLunar() {
+  timerId = setInterval(randomRound, clickToFast);
 }
 
-function movelunar() {
-  timerId = setInterval(randomround, clickToFast);
-}
-
-movelunar();
-
+moveLunar();
 
